0ff198ccdfbce621267ec471e11a3334cd6efa20
Almira Suljovic <almira.suljovic@udg.edu.me>
2019-03-07 13:16:35 +0100

    Homework02

--- task3.py
+++ task3.py
@@ -14,6 +14,21 @@
 ===================================================
 """
 
+def cube_volume():
+    if side_in_cm is not > 1:
+        return False
+    else:
+        return True
+
+def main():
+    if cube_volume(side_in_cm):
+        side_in_cm = 5.0
+        volume_of_cube = cube_volume(side_in_cm)
+        print("Volume of a cube is: ", volume_of_cube)
+    else:
+        print(" Your input is not valid")
+
+main()
 
 
 def main():
